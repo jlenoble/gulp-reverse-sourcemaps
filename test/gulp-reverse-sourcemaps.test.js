4dbc9c35f6235ae2ab064bb1fe5b1d84ce2b40f3
Jason Lenoble <jason.lenoble@gmail.com>
2018-10-08 16:29:21 +0200

    Override onSpawnError
    Gulp@4 expects Babel@7, but upgrading is not priority
    work for now, so inhibit error

--- test/gulp-reverse-sourcemaps.test.js
+++ test/gulp-reverse-sourcemaps.test.js
@@ -2,6 +2,14 @@ import testGulpProcess, {isFound, nextTask} from 'test-gulp-process';
 import equalFileContents from 'equal-file-contents';
 import destglob from 'destglob';
 
+function onSpawnError (err) {
+  if (/Failed to load external module @babel\/register/.test(err.message)) {
+    // Using Babel < 7.0, but it's fine, don't crash because of the noise
+    return;
+  }
+  return Promise.reject(err);
+}
+
 describe('Testing Gulpfile', function () {
   it(`Reverting a transpile task with sourcemaps`, testGulpProcess({
     sources: ['src/**/*.js', 'test/**/*.js', 'gulp/**/*.js'],
@@ -20,6 +28,7 @@ describe('Testing Gulpfile', function () {
         isFound('build2/orig/src/gulp-reverse-sourcemaps.js')],
     ],
 
+    onSpawnError,
     onSuccess () {
       const [glob] = destglob('src/gulp-reverse-sourcemaps.js', this.dest);
       const [dest] = destglob('build2/orig', this.dest);
@@ -44,6 +53,7 @@ describe('Testing Gulpfile', function () {
         isFound('build2/src/gulp-reverse-sourcemaps.js')],
     ],
 
+    onSpawnError,
     onSuccess () {
       const [glob] = destglob('src/gulp-reverse-sourcemaps.js', this.dest);
       const [dest] = destglob('build2', this.dest);
